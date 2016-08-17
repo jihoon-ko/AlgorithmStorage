0a6aa601e1f1a00b36f9683d9b0eb3e080cb9b29
jihoon-ko <jihoonko@kaist.ac.kr>
2016-08-17 21:32:54 +0900

    More faster Dinic algorithm

--- graph/flow/NetworkFlow.cpp
+++ graph/flow/NetworkFlow.cpp
@@ -1,97 +1,96 @@
 #include<cstdio>
 #include<vector>
-#include<stack>
 #include<queue>
-#include<algorithm>
+#include<string.h>
 #define inf 2100000000
 using namespace std;
 
-class edge{
-public:
-    int from,to,flow,next;
+struct maxflow{
+	typedef int flow_t;
+	struct edge{
+		int to;
+		int inv;
+		flow_t flow;
+	};
+	vector< vector<edge> > graph;
+	vector<int> lvl;
+	queue<int> q;
+	int n;
+	int source, sink;
+	void init(int n_){
+		n = n_;
+		graph.resize(n);
+		lvl.resize(n);
+	}
+	void addedge(int from_,int to_,flow_t flow_,flow_t flowrev_=0){
+		edge ftot = {to_,graph[to_].size(),flow_};
+		edge ttof = {from_,graph[from_].size(),flowrev_};
+		graph[from_].push_back(ftot);
+		graph[to_].push_back(ttof);
+	}
+	int setlvl(){
+		for(int i=0;i<n;i++) lvl[i]=inf;
+		q.push(source);
+		lvl[source]=0;
+		while(!q.empty()){
+			int now = q.front(); q.pop();
+			for(int i=0;i<graph[now].size();i++){
+				edge nowedge = graph[now][i];
+				if(nowedge.flow){
+					if(lvl[nowedge.to]^inf) continue;
+					lvl[nowedge.to] = lvl[now] + 1;
+					q.push(nowedge.to);
+				}
+			}
+		}
+		return lvl[sink];
+	}
+	int blockingflow(int now,flow_t can_flow){
+		flow_t res = 0;
+		flow_t retval = 0;
+		if(now==sink) return can_flow;
+		for(int i=0;i<graph[now].size();i++){
+			edge nowedge = graph[now][i];
+			if(nowedge.flow && lvl[nowedge.to] == lvl[now] + 1){
+				retval = blockingflow(nowedge.to,(can_flow-res<nowedge.flow)?(can_flow-res):nowedge.flow);
+				if(retval){
+					res += retval;
+					graph[now][i].flow -= retval;
+					graph[nowedge.to][nowedge.inv].flow += retval;
+				}
+			}
+		}
+		return res;
+	}
+	flow_t dinic(int source_,int sink_){
+		source = source_;
+		sink = sink_;
+		flow_t res = 0;
+		while(setlvl()^inf){
+			res += blockingflow(source,inf); 
+		}
+		return res;
+	}
 };
-
-const int maxn = 5000;
-
-vector<edge> graph;
-int last[maxn+1];
-
-namespace maxflow{
-    queue<int> q;
-    stack<int> stk;
-    int n;
-    int lvl[maxn+1];
-    int visited[maxn+1];
-    int source,sink;
-    void buildlgraph(){
-        int now,target;
-        for(int i=0;i<n;i++) lvl[i]=inf;
-        lvl[source]=0;q.push(source);
-        while(!q.empty()){
-            now=q.front();q.pop();
-            for(int i=last[now];i>=0;i=graph[i].next){
-                if(!graph[i].flow) continue;
-                if(lvl[graph[i].to]^inf) continue;
-                target=graph[i].to;
-                lvl[target]=lvl[now]+1;
-                q.push(target);
-            }
-        }
-        /*
-        for(int i=0;i<n;i++) printf("%d ",lvl[i]);
-        printf("\n");
-        */
-    }
-    int blockingflow(int now,int pedge,int lim){
-        if(!lim) return 0;
-        int res = 0;
-        visited[now]=1;
-        if(now!=sink){
-            for(int i=last[now];i>=0;i=graph[i].next){
-                if(!graph[i].flow) continue;
-                if(visited[graph[i].to]) continue;
-                if(lvl[graph[i].to]==lvl[now]+1){
-                    res += blockingflow(graph[i].to,i,min(lim-res,graph[i].flow));
-                    if(lim==res) break;
-                }
-            }
-        }else res = lim;
-        visited[now]=0;
-        if(pedge>=0){
-            graph[pedge].flow -= res;
-            graph[pedge^1].flow += res;
-        }
-        return res;
-    }
-    int dinic(int sz,int src,int snk){
-        int res=0;n=sz;source=src;sink=snk;
-        while(1){
-            buildlgraph();
-            if(lvl[sink]^inf) res += blockingflow(0,-1,inf);
-            else break;
-        }
-        return res;
-    }
-};
-
-void BEDGE(int ii,int from,int to,int flow){
-    edge imsi;
-    imsi.from=from;imsi.to=to;imsi.flow=flow;imsi.next=last[from];graph.push_back(imsi);
-    imsi.from=to;imsi.to=from;imsi.flow=0;imsi.next=last[to];graph.push_back(imsi);
-    last[from]=ii*2;last[to]=ii*2+1;
-}
 
 int main(){
-    int edgecnt=0;
-    int t;
-    scanf("%d",&t);
-    while(t--){
-        edgecnt=0;
-        for(int i=0;i<n*m+2;i++){
-            last[i]=-1;
-        }
-        //BEDGE  something(edgecnt++,from,to,flow)
-        printf("%d\n",maxflow::dinic(n*m+2,0,n*m+1));
-        graph.clear();
-    }
+	setbuf(stdout,NULL);
+	maxflow mf=maxflow();
+	int n,m,aa,bb,k;
+	scanf("%d %d %d",&n,&m,&k);
+	mf.init(n+m+3);
+	mf.addedge(0,n+m+2,k);
+	for(int i=1;i<=n;i++){
+		mf.addedge(0,i,1);
+		mf.addedge(n+m+2,i,inf);
+		scanf("%d",&aa);
+		while(aa--){
+			scanf("%d",&bb);
+			mf.addedge(i,n+bb,1);
+		}
+	}
+	for(int i=n+1;i<=n+m;i++){
+		mf.addedge(i,n+m+1,1);
+	}
+	printf("%d\n",mf.dinic(0,n+m+1));
 }
